8f1fcaea14c3349aa4c5f1f9b5be819ddc2ca20d
Erick Roen <ErRoen@users.noreply.github.com>
2019-11-03 16:28:42 -0600

    Update CommonDevTools.ps1
    Updating DevTools

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -5,3 +5,28 @@ choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTermi
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
+
+# Dev tools
+choco install -y notepadplusplus
+choco install -y dotpeek
+choco install -y logparser
+choco install -y fiddler
+choco install -y beyondcompare
+choco install -y paint.net
+choco install -y postman
+choco install -y soapui
+choco install -y smtp4dev
+
+choco install -y sql-server-management-studio
+
+# VSCode plugins ???
+
+# Utilities
+choco install -y webpi
+choco install -y 7zip
+choco install -y grepwin
+choco install -y filezilla
+choco install -y hashcheck
+choco install -y windirstat
+choco install -y foxitreader
+choco install -y divvy
