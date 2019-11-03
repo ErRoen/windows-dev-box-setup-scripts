db06acf0f0689c604c6b1976c6228aae8d1ec6dc
Erick Roen <ErRoen@users.noreply.github.com>
2019-11-03 16:29:05 -0600

    Moving VS to own file

--- dev_app_desktop_.NET.ps1
+++ dev_app_desktop_.NET.ps1
@@ -45,18 +45,7 @@ choco install -y terraform
 write-host "Installing tools inside the WSL distro..."
 Ubuntu1804 run apt install ansible -y
 
-#--- Tools ---
-#--- Installing VS and VS Code with Git
-# See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
-# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
-# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
-choco install -y visualstudio2019professional --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
-
-Update-SessionEnvironment #refreshing env due to Git install
-
-#--- UWP Workload and installing Windows Template Studio ---
-choco install -y visualstudio2017-workload-azure
-choco install -y visualstudio2017-workload-manageddesktop
+executeScript "VisualStudioSetup.ps1";
 
 #--- reenabling critial items ---
 Enable-UAC
