0919e012edc64d699d0143b3d3ced4a591976ab9
Erick Roen <ErRoen@users.noreply.github.com>
2019-11-04 15:14:10 -0600

    Update VisualStudioSetUp.ps1

--- scripts/VisualStudioSetUp.ps1
+++ scripts/VisualStudioSetUp.ps1
@@ -2,9 +2,9 @@
 # See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
 # https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
 # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
-choco install -y visualstudio2019professional #--package-parameters="--add Microsoft.VisualStudio.Component.Git"
+#choco install -y visualstudio2019professional #--package-parameters="--add Microsoft.VisualStudio.Component.Git"
 
-Update-SessionEnvironment #refreshing env due to Git install
+#Update-SessionEnvironment #refreshing env due to Git install
 
 #--- Workloads and installing Windows Template Studio ---
 #choco install -y visualstudio2019-workload-azure
