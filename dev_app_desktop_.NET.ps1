427a23bb175324bcbcf50a2187fa6a539b03adfd
Erick Roen <ErRoen@users.noreply.github.com>
2019-11-04 13:37:30 -0600

    Update dev_app_desktop_.NET.ps1

--- dev_app_desktop_.NET.ps1
+++ dev_app_desktop_.NET.ps1
@@ -25,28 +25,28 @@ function executeScript {
 executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
-executeScript "CommonDevTools.ps1";
-
 executeScript "Browsers.ps1";
 
 executeScript "HyperV.ps1";
 RefreshEnv
-executeScript "WSL.ps1";
-RefreshEnv
-executeScript "Docker.ps1";
+
+choco install dotnet4.7.2
+choco install dotnetfx
+choco install dotnetcore
+
+executeScript "CommonDevTools.ps1";
 
 choco install -y powershell-core
 choco install -y azure-cli
 Install-Module -Force Az
 choco install -y microsoftazurestorageexplorer
 choco install -y terraform
-
-# Install tools in WSL instance
-write-host "Installing tools inside the WSL distro..."
-Ubuntu1804 run apt install ansible -y
+choco install -y servicebusexplorer
 
 # Install VS and Extensions
-executeScript "VisualStudioSetup.ps1";
+executeScript "VisualStudioSetUp.ps1";
+
+choco install -y sql-server-management-studio
 
 # Pin Items to TaskBar
 Install-ChocolateyPinnedTaskBarItem "$env:programfiles\console\console.exe"
