8dc100d79112a928f4e4d7a4fe0e525b48bac270
Erick Roen <ErRoen@users.noreply.github.com>
2019-11-05 06:16:23 -0600

    Update CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -4,7 +4,6 @@ choco install -y vscode
 choco install -y git --package-parameters="/GitAndUnixToolsOnPath /WindowsTerminal"
 choco install -y 7zip.install
 choco install -y sysinternals
-choco install -y powershell
 
 # Dev tools
 choco install -y notepadplusplus
